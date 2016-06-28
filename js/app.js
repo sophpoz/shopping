a8c8fb89cc8736ec2048fb867167b8618e2d746e
Sophia Pozzi <sopozzi@gmail.com>
2016-06-27 20:31:45 -0700

    updates

--- js/app.js
+++ js/app.js
@@ -1,9 +1,45 @@
 $(document).ready(function(){
+	
+	$('.x').on('click',function() {
+		$(this).parent('li').remove();
+	})
 
-	$('.eggs')mouseenter(function() {
-		$('.x').show();
+	$('#submit').on('click', function(){
+		$('ul').append('<li>' + '<input type="checkbox">' + $('.textbox').val() + ' <button class="x" type="button">x</button>');
+		$('.x').on('click',function() {
+			$(this).parent('li').remove();
+		})
+		$('input:checkbox').on('change', function(){
+		var input = $(this).parent('li');
+		if (this.checked){
+			$(input).css({
+				'textDecoration': 'line-through',
+				'color': 'grey'
+			});
+		} else {
+			$(input).css({
+				'textDecoration': 'none',
+				'color' : 'black'
+			});
+		}
+		})
+		$('.textbox').val('');
+
 	})
-	$('.eggs')mouseleave(function(){
-		$('.x').hide();
+
+	$('input:checkbox').on('change', function(){
+		var input = $(this).parent('li');
+		if (this.checked){
+			$(input).css({
+				'textDecoration': 'line-through',
+				'color': 'grey'
+			});
+		} else {
+			$(input).css({
+				'textDecoration': 'none',
+				'color' : 'black'
+			});
+		}
 	})
+
 });
