b937616fa6c1989ef6f286261a72fbb6c03d5a9d
Sophia Pozzi <sopozzi@gmail.com>
2016-06-30 08:09:13 -0700

    editable

--- js/app.js
+++ js/app.js
@@ -1,30 +1,16 @@
 $(document).ready(function(){
-	$('#submit').on('click', function(){
-		$('ul').append('<li>' + '<input type="checkbox">' + $('.textbox').val() + ' <button class="x" type="button">x</button>');
-		$('.x').on('click',function() {
-			$(this).parent('li').remove();
-		})
-		$('input:checkbox').on('change', function(){
-		var input = $(this).parent('li');
-		if (this.checked){
-			$(input).css({
-				'textDecoration': 'line-through',
-				'color': 'grey'
-			});
-		} else {
-			$(input).css({
-				'textDecoration': 'none',
-				'color' : 'black'
-			});
-		}
-		})
+	$('#newLi').submit(function(e){
+		e.preventDefault();
+		if( $('#textbox').val().length === 0 ){return false}
+		$('ul').append('<li>' + '<input type="checkbox"><span>' + $('.textbox').val() + '</span><button class="x" type="button">x</button>');
 		$('.textbox').val('');
-
 	})
-});
-$(document).keypress(function(e){
-	if(e.which === 13) {
-		$('ul').append('<li>' + '<input type="checkbox">' + $('.textbox').val() + ' <button class="x" type="button">x</button>');
+	//click on li element, allows you to edit
+	$('#toDoList').click(function(e){
+		if(e.target.tagName === 'SPAN'){	
+			var thisLi = e.target;
+			$(thisLi).attr('contenteditable', true)
+		}
 		$('.x').on('click',function() {
 			$(this).parent('li').remove();
 		})
@@ -42,7 +28,7 @@ $(document).keypress(function(e){
 			});
 		}
 		})
-		$('.textbox').val('');
-	}
-	// if('#textbox').val().length() === 0 ){return false}
+	})
+	
 })
+
